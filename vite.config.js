578ab3493b42e1ad2756ef8078bea183e06a1cce
AMrCEg <eng.amrnegm@gmail.com>
2024-08-11 12:57:14 +0300

    modified some files

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: "https://github.com/AMrCEg/todo-react.git",
   plugins: [react()],
+  base: "todo-react.git",
 });
